5a2299e099e52b388701c74bbbc3c5eb33c3806a
Justin Farley <justin.farley94@gmail.com>
2022-03-29 12:21:59 -0400

    Item Details Update
    Added in some basic GET/POST/PUT statements for Receiving inventory into the system. Note, for the mass POST, we probably need to do a transactional, where everything succeeds or nothing succeeds.

--- src/main/java/com/ims/server/controller/ItemDetailsController.java
+++ src/main/java/com/ims/server/controller/ItemDetailsController.java
@@ -1,7 +1,13 @@
 package com.ims.server.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +28,43 @@ public class ItemDetailsController {
 	@GetMapping(path = "/item-details")
 	public @ResponseBody Iterable<ItemDetails> getAllItems() {
 		return itemDetailsRepo.findAll();
+	}
+	
+	@PostMapping(path = "/item-details")
+	public ItemDetails newItem(@RequestBody ItemDetails item) {
+		// Note, this only handles one item at a time, we need to have it handle multiple items at a single time
+		// Note, this also does not receive the id of the actual item, so we have to manually put that in for now
+		item.setItemId(1); // TESTING FOR NOW, HARD-CODED
+		return itemDetailsRepo.save(item);
+	}
+	
+	@PostMapping(path = "/item-details/receive")
+	public String recieveItems(@RequestBody List<ItemDetails> items) {
+		for (ItemDetails item : items) {
+			try {
+				itemDetailsRepo.save(item);
+			} catch (Exception e) {
+				return "Failed"; // THIS SHOULD PROBABLY BE A TRANSACTIONAL I BET!
+			}
+		}
+		return "Success";
+	}
+	
+	// Note, there is a bug with this, it automatically updates everything that is not set to null
+	@PutMapping(path = "/item-details")
+	public ItemDetails ItemDetails(@RequestBody ItemDetails item, @RequestParam Long id) {
+		return itemDetailsRepo.findById(id).map(itemdetails -> {
+			itemdetails.setLocation(item.getLocation());
+			itemdetails.setOrderNumber(item.getOrderNumber());
+			itemdetails.setExpiration(item.getExpiration());
+			itemdetails.setDateReceived(item.getDateReceived());
+			itemdetails.setDateShipped(item.getDateShipped());
+			itemdetails.setDateDelivered(item.getDateDelivered());
+			return itemDetailsRepo.save(itemdetails);
+		}).orElseGet(() -> {
+			System.out.println("We tried to update a item, but it didn't exist, so we are creating it");
+			item.setItemId(1); // TESTING FOR NOW, HARD-CODED, NEED TO AUTOMATICALLY GET THIS
+			return itemDetailsRepo.save(item);
+		});
 	}
 }
--- src/main/java/com/ims/server/model/ItemDetails.java
+++ src/main/java/com/ims/server/model/ItemDetails.java
@@ -3,10 +3,14 @@ package com.ims.server.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "item_details")
 public class ItemDetails {
 
 	@Id
@@ -14,16 +18,24 @@ public class ItemDetails {
 	private Long id;
 	private long itemId;
 	
-	@Column(name = "date_receieved")
+	// NOTE THAT THE DATE TYPE IS:
+	// yyyy-mm-dd'T'HH:mm:ss.sss
+	
+	@Column(name = "date_received")
 	private Date dateReceived;
+	
 	@Column(name = "order_number")
 	private String orderNumber;
+	
 	private String location;
 	private Date expiration;
+	
 	@Column(name = "date_shipped")
 	private Date dateShipped;
+	
 	@Column(name = "date_delivered")
 	private Date dateDelivered;
+	
 	public Long getId() {
 		return id;
 	}
