78381f3a45548c6ee047b335ca0a7b115a32ed9c
Justin Farley <justin.farley94@gmail.com>
2022-04-01 14:05:18 -0400

    Updated Location to support dimensions and warehouses

--- src/main/java/com/ims/server/model/Location.java
+++ src/main/java/com/ims/server/model/Location.java
@@ -19,22 +19,22 @@ public class Location {
 	@Column(name = "location_name")
 	private String locationName;
 	
-	@Column(name = "address_1")
-	private String addressOne;
-	
-	@Column(name = "address_2")
-	private String addressTwo;
-	
-	private String city;
-	private String state;
-	private String country;
-	private String zip;
-	
 	@Column(name = "date_created")
 	private Date dateCreated;
 	
 	@Column(name = "date_deleted")
 	private Date dateDeleted;
+	
+	private Double width;
+	private Double height;
+	private Double length;
+	private Double weight;
+	
+	@Column(name = "weight_max")
+	private Double weightMax;
+	
+	@Column(name = "warehouse_id")
+	private Long warehouseId;
 
 	public Long getId() {
 		return id;
@@ -52,54 +52,6 @@ public class Location {
 		this.locationName = locationName;
 	}
 
-	public String getAddressOne() {
-		return addressOne;
-	}
-
-	public void setAddressOne(String addressOne) {
-		this.addressOne = addressOne;
-	}
-
-	public String getAddressTwo() {
-		return addressTwo;
-	}
-
-	public void setAddressTwo(String addressTwo) {
-		this.addressTwo = addressTwo;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getZip() {
-		return zip;
-	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-
 	public Date getDateCreated() {
 		return dateCreated;
 	}
@@ -114,6 +66,54 @@ public class Location {
 
 	public void setDateDeleted(Date dateDeleted) {
 		this.dateDeleted = dateDeleted;
+	}
+
+	public Double getWidth() {
+		return width;
+	}
+
+	public void setWidth(Double width) {
+		this.width = width;
+	}
+
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+	public Double getLength() {
+		return length;
+	}
+
+	public void setLength(Double length) {
+		this.length = length;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Double getWeightMax() {
+		return weightMax;
+	}
+
+	public void setWeightMax(Double weightMax) {
+		this.weightMax = weightMax;
+	}
+
+	public Long getWarehouseId() {
+		return warehouseId;
+	}
+
+	public void setWarehouseId(Long warehouseId) {
+		this.warehouseId = warehouseId;
 	}
 	
 	
